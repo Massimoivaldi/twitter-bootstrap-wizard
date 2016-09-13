d2899b33d903c70053385887ac98dbc82c8a745a
MarketingMix <info@marketingmix.tf>
2016-09-13 08:46:11 +0000

    ...

--- custom.js
+++ custom.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
         var rubrica = {"0":"Scegli","1":"Necrologie"};
         var defunto = {"0":"","1":"Nino Bixio","2":"Giuseppe Mazzini"};
         var gradopar = {"0":"","1":"Amici","2":"Colleghi","3":"Coniuge","4":"Conoscente"};
+        var tipoannuncio = {"0":"Scegli","1":"Adesione/Partecipazione","2":"Anniversario","3":"Famiglia/Annuncio","4":"Ringraziamento/Trigesimo"};
         
         
     	/* -------------------------------------------- */
@@ -17,6 +18,7 @@ $(document).ready(function() {
         fillselect($('#testata'),testate);
         fillselect($('#defunto'),defunto);
         fillselect($('#gradopar'),gradopar);
+        fillselect($('#tipoannuncio'),tipoannuncio);
         
         $("#testata").change(function(){
             var testataid = $('#testata').val();
@@ -44,6 +46,17 @@ $(document).ready(function() {
                 }
         })
         
+        $("#tipoannuncio").change(function(){
+            var edizioneid = $('#tipoannuncio').val();
+            //alert('suca');
+                if(edizioneid == 1){
+                    $("#fotoadesione").css("display","block")
+                } else {
+                    $("#fotoadesione").css("display","none")
+                }
+                
+        })
+        
         
         
         
@@ -58,7 +71,7 @@ $(document).ready(function() {
 			}
 
 			// Set the name for the next tab
-			$('#tab3').html('Hello, ' + $('#name').val());
+		//	$('#tab3').html('Hello, ' + $('#name').val());
 
 		}, onTabShow: function(tab, navigation, index) {
 			var $total = navigation.find('li').length;
@@ -80,4 +93,69 @@ $(document).ready(function() {
         }
 	
 	/* -------------------------------------------- */
+	
+	$("#citazione").keyup(function(event) {
+      var stt = $(this).val();
+      $("#ante-citazione").text(stt);
+      $("#riep-citazione").text(stt);
+    });
+    
+    $("#firmacitazione").keyup(function(event) {
+      var stt = $(this).val();
+      $("#ante-firmacitazione").text(stt);
+      $("#riep-firmacitazione").text(stt);
+    });
+    
+    $("#testoiniziale").keyup(function(event) {
+      var stt = $(this).val();
+      $("#ante-testoiniziale").text(stt);
+      $("#riep-testoiniziale").text(stt);
+    });
+    
+    $("#onorificienza").keyup(function(event) {
+      var stt = $(this).val();
+      $("#ante-onorificienza").text(stt);
+      $("#riep-onorificienza").text(stt);
+    });
+    
+    $("#nomecomdefunto").keyup(function(event) {
+      var stt = $(this).val();
+      $("#ante-nomecomdefunto").text(stt);
+      $("#riep-nomecomdefunto").text(stt);
+    });
+    
+    $("#vedovanza").keyup(function(event) {
+      var stt = $(this).val();
+      $("#ante-vedovanza").text(stt);
+      $("#riep-vedovanza").text(stt);
+    });
+    
+    $("#dianni").keyup(function(event) {
+      var stt = $(this).val();
+      $("#ante-dianni").text(stt);
+      $("#riep-dianni").text(stt);
+    });
+    
+    $("#testoinserzione").keyup(function(event) {
+      var stt = $(this).val();
+      $("#ante-testoinserzione").text(stt);
+      $("#riep-testoinserzione").text(stt);
+    });
+    
+    $("#listanomi").keyup(function(event) {
+      var stt = $(this).val();
+      $("#ante-listanomi").text(stt);
+      $("#riep-listanomi").text(stt);
+    });
+    
+    $("#luogodata").keyup(function(event) {
+      var stt = $(this).val();
+      $("#ante-luogodata").text(stt);
+      $("#riep-luogodata").text(stt);
+    });
+    
+    
+    
+    
+    
 });
